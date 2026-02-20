d2c09ecdc32a9e034a92a232de4ff6510d0b06bf
Fmoosavi07 <fmoosavi842@gmail.com>
2026-02-20 16:17:24 +0330

    left sidebar and buttons for block groups

--- main.cpp
+++ main.cpp
@@ -8,15 +8,18 @@
 #include "Sprite.h"
 #include "Block.h"
 #include "FileHandler.h"
+#include <SDL2/SDL2_gfx.h>
+#include <SDL2/SDL_mixer.h>
 
 
-void renderText(SDL_Renderer* ren, TTF_Font* font, std::string text, int x, int y, SDL_Color color = {255, 255, 255, 255}) {
+
+void renderText(SDL_Renderer* ren, TTF_Font* font, std::string text, int x, int y, SDL_Color color) {
     if (!font) return;
     SDL_Surface* surf = TTF_RenderText_Blended(font, text.c_str(), color);
     if (!surf) return;
     SDL_Texture* tex = SDL_CreateTextureFromSurface(ren, surf);
     SDL_Rect dst = {x, y, surf->w, surf->h};
-    SDL_RenderCopy(ren, tex, NULL, &dst);
+    SDL_RenderCopy(ren, tex, nullptr, &dst);
     SDL_FreeSurface(surf);
     SDL_DestroyTexture(tex);
 }
@@ -25,7 +28,7 @@ int main(int argc, char* argv[]) {
     SDL_Init(SDL_INIT_VIDEO);
     TTF_Init();
 
-    SDL_Window* win = SDL_CreateWindow("Scratch Pro - Drag & Drop", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1024, 768, SDL_WINDOW_SHOWN);
+    SDL_Window* win = SDL_CreateWindow("Scratch Pro - Drag & Drop", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1920, 1080, 0);
     SDL_Renderer* ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
 
     TTF_Font* font = TTF_OpenFont("assets/arial.ttf", 16);
@@ -33,13 +36,13 @@ int main(int argc, char* argv[]) {
 
 
     std::vector<VisualBlock> sidebarTemplates = {
-        {{MOVE, 40, 0}, {15, 60, 120, 40}, {76, 151, 255, 255}, "MOVE 40"},
-        {{TURN, 15, 0}, {15, 110, 120, 40}, {76, 151, 255, 255}, "TURN 15"},
-        {{PEN_DOWN, 0, 0}, {15, 160, 120, 40}, {64, 184, 158, 255}, "PEN DOWN"},
-        {{PEN_UP, 0, 0}, {15, 210, 120, 40}, {64, 184, 158, 255}, "PEN UP"},
-        {{ERASE, 0, 0}, {15, 260, 120, 40}, {255, 102, 102, 255}, "ERASE"},
-        {{REPEAT, 4, 0}, {15, 310, 120, 40}, {255, 171, 25, 255}, "REPEAT 4"},
-        {{END_LOOP, 0, 0}, {15, 360, 120, 40}, {255, 171, 25, 255}, "END LOOP"}
+        {{MOVE, 40, 0}, {95, 60, 120, 40}, {76, 151, 255, 255}, "MOVE 40"},
+        {{TURN, 15, 0}, {95, 110, 120, 40}, {76, 151, 255, 255}, "TURN 15"},
+        {{PEN_DOWN, 0, 0}, {95, 160, 120, 40}, {64, 184, 158, 255}, "PEN DOWN"},
+        {{PEN_UP, 0, 0}, {95, 210, 120, 40}, {64, 184, 158, 255}, "PEN UP"},
+        {{ERASE, 0, 0}, {95, 260, 120, 40}, {255, 102, 102, 255}, "ERASE"},
+        {{REPEAT, 4, 0}, {95, 310, 120, 40}, {255, 171, 25, 255}, "REPEAT 4"},
+        {{END_LOOP, 0, 0}, {95, 360, 120, 40}, {255, 171, 25, 255}, "END LOOP"}
     };
 
     std::vector<VisualBlock> workspaceBlocks;
@@ -47,7 +50,7 @@ int main(int argc, char* argv[]) {
     int dragOffsetX = 0, dragOffsetY = 0;
 
     Sprite cat;
-    cat.loadBMP(ren, "cat.bmp");
+     cat.loadBMP(ren, "assets/cat.bmp");
     ProgramManager manager;
     int currentStep = 0;
     bool isRunning = false, quit = false;
@@ -86,7 +89,7 @@ int main(int argc, char* argv[]) {
                 }
 
 
-                SDL_Rect runBtn = {15, 600, 120, 40};
+                SDL_Rect runBtn = {95, 600, 120, 40};
                 if (SDL_PointInRect(&p, &runBtn)) {
                     manager.script.clear();
 
@@ -123,7 +126,7 @@ int main(int argc, char* argv[]) {
                             activeDragBlock->rect.x = other.rect.x;
                             activeDragBlock->rect.y = other.rect.y + 40;
                             break;
-                        }
+                            }
                     }
                     activeDragBlock->isDragging = false;
                 }
@@ -143,19 +146,41 @@ int main(int argc, char* argv[]) {
         SDL_RenderClear(ren);
 
 
-        SDL_Rect sideBarRect = {0, 0, 150, 768};
+        SDL_Rect sideBarRect = {80, 0, 150, 1080};
         SDL_SetRenderDrawColor(ren, 200, 200, 200, 255);
         SDL_RenderFillRect(ren, &sideBarRect);
 
-        SDL_Rect stageRect = {800, 0, 224, 768};
+        SDL_Rect stageRect = {800, 0, 224, 1080};
         SDL_SetRenderDrawColor(ren, 230, 230, 230, 255);
         SDL_RenderFillRect(ren, &stageRect);
+
+
+        filledEllipseRGBA(ren, 40, 60, 15, 15, 60, 131, 253, 255);
+        filledEllipseRGBA(ren, 40, 120, 15, 15, 176, 60, 254, 255);
+        filledEllipseRGBA(ren, 40, 180, 15, 15, 251, 60, 250, 255);
+        filledEllipseRGBA(ren, 40, 240, 15, 15, 255, 192, 0, 255);
+        filledEllipseRGBA(ren, 40, 300, 15, 15, 255, 153, 0, 255);
+        filledEllipseRGBA(ren, 40, 360, 15, 15, 0, 220, 255, 255);
+        filledEllipseRGBA(ren, 40, 420, 15, 15, 28, 216, 8, 255);
+        filledEllipseRGBA(ren, 40, 480, 15, 15, 255, 114, 0, 255);
+        filledEllipseRGBA(ren, 40, 540, 15, 15, 255, 62, 142, 255);
+
+        renderText(ren, font, "Motion", 20, 80, {0, 0, 0, 255});
+        renderText(ren, font, "Looks", 20, 140, {0, 0, 0, 255});
+        renderText(ren, font, "Sound", 17, 200, {0, 0, 0, 255});
+        renderText(ren, font, "Events", 16, 260, {0, 0, 0, 255});
+        renderText(ren, font, "Control", 13, 320, {0, 0, 0, 255});
+        renderText(ren, font, "Sensing", 12, 380, {0, 0, 0, 255});
+        renderText(ren, font, "Operators", 10, 440, {0, 0, 0, 255});
+        renderText(ren, font, "Variables", 10, 500, {0, 0, 0, 255});
+        renderText(ren, font, "My Blocks", 7, 560, {0, 0, 0, 255});
+
 
 
         for (auto& b : sidebarTemplates) {
             SDL_SetRenderDrawColor(ren, b.color.r, b.color.g, b.color.b, 255);
             SDL_RenderFillRect(ren, &b.rect);
-            renderText(ren, font, b.label, b.rect.x + 10, b.rect.y + 10);
+            renderText(ren, font, b.label, b.rect.x + 10, b.rect.y + 10, {255, 255, 255, 255});
         }
 
 
@@ -164,14 +189,14 @@ int main(int argc, char* argv[]) {
             SDL_RenderFillRect(ren, &b.rect);
             SDL_SetRenderDrawColor(ren, 0, 0, 0, 255);
             SDL_RenderDrawRect(ren, &b.rect); // Outline
-            renderText(ren, font, b.label, b.rect.x + 10, b.rect.y + 10);
+            renderText(ren, font, b.label, b.rect.x + 10, b.rect.y + 10, {255, 255, 255, 255});
         }
 
 
-        SDL_Rect runBtnRect = {15, 600, 120, 40};
+        SDL_Rect runBtnRect = {95, 600, 120, 40};
         SDL_SetRenderDrawColor(ren, 50, 200, 50, 255);
         SDL_RenderFillRect(ren, &runBtnRect);
-        renderText(ren, font, "GO / RUN", 35, 610);
+        renderText(ren, font, "GO / RUN", 115, 610, {255, 255, 255, 255});
 
         cat.draw(ren);
         SDL_RenderPresent(ren);
