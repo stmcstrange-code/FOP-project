5b1e9ca9235880be726de76bee234d1c1b1f5ce5
stmcstrange-code <stmc.strange@gmail.com>
2026-02-20 11:49:06 +0330

    filehandler update

--- FileHandler.cpp
+++ FileHandler.cpp
@@ -1,24 +1,44 @@
 #include "FileHandler.h"
+#include <iostream>
 
-void saveProject(const std::vector<Block>& script, const std::string & path) {
+void saveProject(const std::vector<VisualBlock>& workspace, const std::string& path) {
     std::ofstream file(path);
     if (file.is_open()) {
-        for (const auto& b : script) {
-            file << static_cast<int>(b.type) << " " << b.value << "\n";
+        for (const auto& vb : workspace) {
+
+            file << static_cast<int>(vb.data.type) << " "
+                 << vb.data.value << " "
+                 << vb.rect.x << " "
+                 << vb.rect.y << "\n";
         }
         file.close();
+        std::cout << "Project saved successfully to " << path << std::endl;
     }
 }
 
-void loadProject(std::vector<Block>& script, const std::string & path) {
-    script.clear();
+void loadProject(std::vector<VisualBlock>& workspace, const std::string& path) {
+    workspace.clear();
     std::ifstream file(path);
     if (file.is_open()) {
-        int t;
+        int t, x, y;
         float v;
-        while (file >> t >> v) {
-            script.push_back({static_cast<BlockType>(t), v});
+
+        while (file >> t >> v >> x >> y) {
+            VisualBlock vb;
+
+            vb.data.type = static_cast<BlockType>(t);
+            vb.data.value = v;
+            vb.data.iterations = 0;
+
+
+            vb.rect = {x, y, 120, 40};
+            vb.isDragging = false;
+
+
+
+            workspace.push_back(vb);
         }
         file.close();
+        std::cout << "Project loaded successfully from " << path << std::endl;
     }
 }
--- FileHandler.h
+++ FileHandler.h
@@ -2,14 +2,13 @@
 
 #ifndef FOP_PROJECT_FILEHANDLER_H
 #define FOP_PROJECT_FILEHANDLER_H
-
 #include <vector>
 #include <string>
 #include <fstream>
 #include "Block.h"
 
-void saveProject(const std::vector<Block>& script, const std::string & path);
-void loadProject(std::vector<Block>& script, const std::string & path);
 
+void saveProject(const std::vector<VisualBlock>& workspace, const std::string& path);
+void loadProject(std::vector<VisualBlock>& workspace, const std::string& path);
 
 #endif //FOP_PROJECT_FILEHANDLER_H
