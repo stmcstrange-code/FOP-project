212760118ad37144c5f980ff8538a2cae5759f20
stmcstrange-code <stmc.strange@gmail.com>
2026-02-25 10:59:35 +0330

    checked stage boundry

--- main.cpp
+++ main.cpp
@@ -490,6 +490,7 @@ int main(int argc, char* argv[]) {
 
         if (isRunning && currentStep < (int)manager.script.size()) {
             executeNext(manager, cat, meow, currentStep);
+            cat.checkBoundaries(1024, 480, 650);
             SDL_Delay(100);
         } else isRunning = false;
 
